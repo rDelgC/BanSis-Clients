1287925322a99435212b0afb1ebcb5dc5b28713e
rDelgC <rdelgado.dev@gmail.com>
2022-03-18 13:58:32 -0500

    bootstrap configuration file added, eureka client enabled

--- src/main/java/com/boot/bansis/client/BansisClientApplication.java
+++ src/main/java/com/boot/bansis/client/BansisClientApplication.java
@@ -2,10 +2,12 @@ package com.boot.bansis.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 
 @SpringBootApplication
 @EnableReactiveMongoAuditing
+@EnableEurekaClient
 public class BansisClientApplication {
 
 	public static void main(String[] args) {
