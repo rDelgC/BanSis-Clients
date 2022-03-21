7f1f604fc0e8c5ddb19cf1206130c7a3c6ba48be
rDelgC <rdelgado.dev@gmail.com>
2022-03-21 14:21:10 -0500

    database configuration added

--- src/main/java/com/boot/bansis/client/entities/Address.java
+++ src/main/java/com/boot/bansis/client/entities/Address.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 /**
@@ -16,9 +15,6 @@ import org.springframework.data.mongodb.core.mapping.Field;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Address {
-
-    @Id
-    private String id;
 
     @Field("desc")
     private String description;
